29ca368ec9f9dfd93a60a50a1d0da197ee0ca945
Mr Sticky Piston <97804691+MrStickyPiston@users.noreply.github.com>
2023-07-12 08:15:47 +0200

    Fixed repos

--- src/data.js
+++ src/data.js
@@ -95,7 +95,7 @@ export const resume = null;
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["portfolio", "typegreek-windows", "no-banscreen-mod"];
+export const filteredProjects = ["pythonDevInstaller", "CGBNvote", "C-programs"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 13-14)
 export const projectCardImages = [
